398583a61e7ba488a2b613cc8515f343e1eb0578
Htet Naing <htetnaing0814@gmail.com>
2022-09-22 15:36:42 +0630

    changed file name

--- my_list.rb
+++ my_list.rb
@@ -1,7 +1,13 @@
-require_relative 'enumerable'
+require_relative './enumerable'
 
 class MyList
+  def initialize(*args)
+    @list = args
+  end
 
-include MyEnumerable
+  include MyEnumerable
 
+  def each(&block)
+    @list.each { |x| block.call(x) }
+  end
 end
